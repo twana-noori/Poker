4327b6deb3b3090a456da155d51621877636225b
twananoori <twananoori.se@gmail.com>
2022-01-20 14:59:58 +0100

    Cleaning up

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,5 @@
 public class Main {
     public static void main (String[] args){
         new PlayCards(2,1,false);
-        new PlayCards(2,1,false);
-    }
-    public static void printBook(Book book){
-        System.out.println("Bokens titel är: " + book.title + ", skriven av: " + book.author + ". Pris: " + book.price +":-");
-    }
-    public static void printMovie(Movie movie){
-        System.out.println("Filmens titel är: " + movie.title + " och är en: " + movie.gengre + ". Pris: " + movie.price +":-");
     }
 }
