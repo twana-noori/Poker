faf54137b350fbdd61c18668f07bd96b9eae8dd7
twananoori <twananoori.se@gmail.com>
2022-01-20 14:36:07 +0100

    Cleaned out

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,6 @@ public class Main {
     public static void main (String[] args){
         new PlayCards(2,1,false);
         new PlayCards(2,1,false);
-        //new StretchTask1("Hejsan Svejsan");
-        //new StretchTask2(600, 2);
-        //String[] persons = {"Kalle", "Ito", "Joshua", "Jasmine", "Kajsa", "Lucas", "Fia", "Ahmed"};
-        //new StretchTask3(persons, 6);
-        //new Task5Collection("Hur månda unika chars i texten");
-        /*Book[] books = new Book[2];
-
-        books[0] = new Book("Boken som inte ville bli läst", "David Sundin", 320);
-        books[1] = new Book("Snabba cash", "Jens Lampidus", 220);
-
-       for(Book book :books){
-           printBook(book);
-       }
-
-        Movie movie = new Movie("Star wars", Gengre.ACTION, 189);
-        Movie movie1 = new Movie("Titanic", Gengre.DRAMA, 289);
-
-*/
     }
     public static void printBook(Book book){
         System.out.println("Bokens titel är: " + book.title + ", skriven av: " + book.author + ". Pris: " + book.price +":-");
