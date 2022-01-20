d0d7448dbe6d0907682d895098d94f2a1ddfdae6
twananoori <twananoori.se@gmail.com>
2022-01-20 11:55:15 +0100

    Commited thru terminal

---
+++ src/PlayingCard.java
@@ -0,0 +1,26 @@
+public class PlayingCard {
+    private int value;
+    private CardType type;
+    private boolean isShown = false;
+
+    public PlayingCard(int value, CardType type){
+        this.value = value;
+        this.type = type;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public CardType getType() {
+        return type;
+    }
+
+    public boolean isShown() {
+        return isShown;
+    }
+
+    public void setShown(boolean shown) {
+        isShown = shown;
+    }
+}
